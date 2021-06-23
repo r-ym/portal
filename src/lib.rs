f0ed771495f2852689d971ae5499e3ff947fc104
r-ym <ramanyachi@gmail.com>
2021-06-24 01:56:05 +0900

    dman

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ use bevy::{
     prelude::*,
     render::{camera::Camera, render_graph::base::camera::CAMERA_3D},
 };
-use rand::Rng;
 use wasm_bindgen::prelude::*;
 
 #[derive(Clone, Eq, PartialEq, Debug, Hash)]
@@ -66,8 +65,8 @@ struct Game {
     camera_is_focus: Vec3,
 }
 
-const BOARD_SIZE_I: usize = 14;
-const BOARD_SIZE_J: usize = 21;
+const BOARD_SIZE_I: usize = 4;
+const BOARD_SIZE_J: usize = 8;
 
 const RESET_FOCUS: [f32; 3] = [
     BOARD_SIZE_I as f32 / 2.0,
@@ -80,8 +79,8 @@ fn setup_cameras(mut commands: Commands, mut game: ResMut<Game>) {
     game.camera_is_focus = game.camera_should_focus;
     commands.spawn_bundle(PerspectiveCameraBundle {
         transform: Transform::from_xyz(
-            -(BOARD_SIZE_I as f32 / 2.0),
-            2.0 * BOARD_SIZE_J as f32 / 3.0,
+            -(BOARD_SIZE_I as f32 / 4.0),
+            2.0 * BOARD_SIZE_J as f32 / 4.0,
             BOARD_SIZE_J as f32 / 2.0 - 0.5,
         )
         .looking_at(game.camera_is_focus, Vec3::Y),
@@ -108,7 +107,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut game: ResMu
         .map(|j| {
             (0..BOARD_SIZE_I)
                 .map(|i| {
-                    let height = rand::thread_rng().gen_range(-0.1..0.1);
+                    let height = 0.0;
                     commands
                         .spawn_bundle((
                             Transform::from_xyz(i as f32, height - 0.2, j as f32),
@@ -133,13 +132,14 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut game: ResMu
                         game.board[game.player.j][game.player.i].height,
                         game.player.j as f32,
                     ),
+                    // rotation: Quat::from_rotation_y(2.0),
                     rotation: Quat::from_rotation_y(-std::f32::consts::FRAC_PI_2),
                     ..Default::default()
                 },
                 GlobalTransform::identity(),
             ))
             .with_children(|cell| {
-                cell.spawn_scene(asset_server.load("models/AlienCake/alien.glb#Scene0"));
+                cell.spawn_scene(asset_server.load("models/AlienCake/tot.glb#Scene0"));
             })
             .id(),
     );
@@ -165,28 +165,40 @@ fn move_player(
         if game.player.i < BOARD_SIZE_I - 1 {
             game.player.i += 1;
         }
-        rotation = -std::f32::consts::FRAC_PI_2;
+        // else{
+        //     game.player.i = 0;
+        // }
+        rotation = std::f32::consts::FRAC_PI_2;
         moved = true;
     }
     if keyboard_input.just_pressed(KeyCode::Down) {
         if game.player.i > 0 {
             game.player.i -= 1;
         }
-        rotation = std::f32::consts::FRAC_PI_2;
+        // else{
+        //     game.player.i = BOARD_SIZE_I-1;
+        // }
+        rotation = -std::f32::consts::FRAC_PI_2;
         moved = true;
     }
     if keyboard_input.just_pressed(KeyCode::Right) {
         if game.player.j < BOARD_SIZE_J - 1 {
             game.player.j += 1;
         }
-        rotation = std::f32::consts::PI;
+        else{
+            game.player.j = 0;
+        }
+        rotation = 0.0;
         moved = true;
     }
     if keyboard_input.just_pressed(KeyCode::Left) {
         if game.player.j > 0 {
             game.player.j -= 1;
         }
-        rotation = 0.0;
+        else{
+            game.player.j = BOARD_SIZE_J-1;
+        }
+        rotation = std::f32::consts::PI;
         moved = true;
     }
 
